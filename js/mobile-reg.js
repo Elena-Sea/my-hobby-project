ff426877b0a3cd9bbedf58841ef9c675b42ea025
Elena-Sea <o.smetyuk@gmail.com>
2022-10-31 22:39:27 +0100

    Adds array of heroes objects

--- js/mobile-reg.js
+++ js/mobile-reg.js
@@ -29,3 +29,26 @@
     regFormRef.classList.toggle("is-open");
   });
 })();
+
+// scroll-to-top
+
+let btnToTop = document.querySelector("#scroll-to-top");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+btnToTop.addEventListener("click", topFunction);
